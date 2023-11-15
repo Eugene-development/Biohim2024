048ced38a8d8278bc9e18ddf23e9d103f36c3eb3
Eugene Che <indexpro24@gmail.com>
2023-11-15 23:35:38 +0300

    c

--- project/src/UI/Menu/v1/index.jsx
+++ project/src/UI/Menu/v1/index.jsx
@@ -227,6 +227,58 @@ export default function index() {
                     {item.name}
                   </a>
                 ))}
+            <OutsideClickHandler onOutsideClick={handleOutsideClick}>
+							<Popover className="relative">
+								<Popover.Button
+									onClick={() => {
+										setShowServices(!showServices)
+                    
+									}}
+									className="outline-none flex -mx-3 rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+								>
+									Информация
+									<ChevronDownIcon
+										className="h-5 w-5 mt-1 ml-2 flex-none text-lime-500"
+										aria-hidden="true"
+									/>
+								</Popover.Button>
+
+								<Transition
+									show={showServices}
+									as={Fragment}
+									enter="transition ease-out duration-200"
+									enterFrom="opacity-0 translate-y-1"
+									enterTo="opacity-100 translate-y-0"
+									leave="transition ease-in duration-150"
+									leaveFrom="opacity-100 translate-y-0"
+									leaveTo="opacity-0 translate-y-1"
+								>
+									<Popover.Panel className="absolute -left-8 top-full z-10 mt-3 mx-6 w-96 rounded-3xl bg-white p-4 shadow-lg ring-1 ring-gray-900/5">
+										{services.map((item) => (
+											<div
+												onClick={() => setShowServices(false)} 
+                        
+												key={item.name}
+												className="relative rounded-lg p-4 hover:bg-gray-50"
+											>
+												<Link
+                        onClick={() => setMobileMenuOpen(false)}
+													href={item.href}
+													className="block text-sm font-semibold leading-6 text-gray-900"
+												>
+													{item.name}
+													<span className="absolute inset-0" />
+												</Link>
+												{/* <p className="mt-1 text-sm leading-6 text-gray-600">
+													{item.description}
+												</p> */}
+											</div>
+										))}
+									</Popover.Panel>
+								</Transition>
+							</Popover>
+						</OutsideClickHandler>
+
               </div>
               <div className="py-6">
                 <a
